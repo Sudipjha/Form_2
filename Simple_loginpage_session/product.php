d61ac9b315f678c1d5262afff41bb3511ef1bddb
Sudipjha <34576447+Sudipjha@users.noreply.github.com>
2020-10-01 10:21:31 +0530

    Add files via upload

---
+++ Simple_loginpage_session/product.php
@@ -0,0 +1,17 @@
+<?php
+
+session_start();
+
+if (isset($_SESSION['uname'])) {
+
+	echo "<h2>Welcome to product page</h2>";
+	
+	echo "<br><a href='welcome.php'><input type=button value=back name=back></a>";
+}
+
+else{
+	
+		 echo "<script>location.href='login.php'</script>";
+	}
+
+?>
